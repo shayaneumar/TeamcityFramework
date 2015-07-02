fdf7eb3029ac18de59dae40daaf8a224710a3810
Shayane Umar <shayane.umar@jci.com>
2015-07-02 10:19:11 +0530

    initial commit

---
+++ DemoServer/Printer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoServer
+{
+    public class Printer
+    {
+        public static void Write()
+        {
+            Console.WriteLine("This is team city test");
+        }
+    }
+}
